c7833f7a2a800e902997a5e86c1a9e5090a941e5
David Shams <david.shams@united.com>
2019-01-10 15:03:26 -0600

    BUG ko is not defined

--- test/appSpec.js
+++ test/appSpec.js
@@ -5,7 +5,18 @@ describe("A suite", function() {
 });
 describe("A suite", function() {
     it("contains spec with an expectation", function() {
-      expect(true).toBe(false);
+      expect(true).toBe(true);
     });
 });
   
+var ViewModel = function (first, last) {
+  this.firstName = ko.observable(first);
+  this.lastName = ko.observable(last);
+
+  this.fullName = ko.pureComputed(function () {
+      // Knockout tracks dependencies automatically. It knows that fullName depends on firstName and lastName, because these get called when evaluating fullName.
+      return this.firstName() + " " + this.lastName();
+  }, this);
+};
+
+ko.applyBindings(new ViewModel("David", "Shams")); // This makes Knockout get to work
